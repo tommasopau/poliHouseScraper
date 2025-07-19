aa01e084dc998c95f36b2f732363945cb85d90a8
tommaso <tommaso.pauselli@mail.polimi.it>
2025-07-19 18:31:47 +0200

    fixed import

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from app.app_factory import create_app
-from app.core.config import config
+from app.core.config import settings
 if __name__ == "__main__":
     app = create_app()
-    uvicorn.run(app, host="0.0.0.0", port=config.PORT)
+    uvicorn.run(app, host="0.0.0.0", port=settings.PORT)
